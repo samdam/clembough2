5eea80384867c3b119c40192faa9a64d2914765c
adam <adamgwilliam@gmail.com>
2012-08-17 14:40:10 -0500

    appending html functionality

--- main.py
+++ main.py
@@ -6,46 +6,27 @@ import alcParse
 import news_info
 import yahoo
 import linked_in
+
 def srch(event, LI_acc_token):
-    print event
-    
     subject = linked_in.api_call(LI_acc_token, event[0])
-    print subject
     
     yahoo_quote = yahoo.comORorg(event[1])
-    print yahoo_quote
     
     news = news_info.NewsInfo(event[0], event[1])
     
     bing_stories = news.getStories()
-    print "Articles from Bing: "
-    if bing_stories == None:
-        print "None found."
-    else:
-        if len(bing_stories) >= 5:
-            for i in range(5):
-                for piece in bing_stories[i]:
-                    print piece
-        else:
-            for story in bing_stories:
-                for piece in story:
-                    print piece
 
-"""    crain_stories = news.getCrainStories(subject.location.string.split()[0])
-    if not crain_stories == None:
-        print "Crain's stories: "
-        for story in crain_stories:
-            print story
-    else:
-        print "No Crain's stories found."
-    """
+    crain_stories = news.getCrainStories(subject.location.string.split()[0])
+
+    return [subject, yahoo_quote, news, bing_stories, crain_stories]
 
 def getEvents():
     event_data = alcParse.main()
+    events = []
     LI_acc_token = linked_in.getOAuthToken()
     for event in event_data:
-        srch(event, LI_acc_token)
-
+        events.append([event, srch(event, LI_acc_token)])
+    return events
 
 def main():
     event_data = alcParse.main()
