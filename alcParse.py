3f71083df8a0b8d9c4be6c1c7437b39ce5e6f8f3
samdam <adamgwilliam@gmail.com>
2012-08-21 11:51:40 -0500

    Update alcParse.py

--- alcParse.py
+++ alcParse.py
@@ -47,7 +47,7 @@ def getCompanies(s):
 
 # list of stopwords used in getCompanies
 stopwords = ['a','able','about','across','after','all','almost','also','am',
-             'among','an','and','any','are','as','at','be','because','been',
+             'among','an','any','are','as','at','be','because','been',
              'but','by','can','cannot','could','dear','did','do','does',
              'either','else','ever','every','for','from','get','got','had',
              'has','have','he','her','hers','him','his','how','however','i',
@@ -55,7 +55,7 @@ stopwords = ['a','able','about','across','after','all','almost','also','am',
              'let','like','likely','may','me','might','most','must','my',
              'neither','no','nor','not','off','often','on','only','or',
              'other','our','own','rather','said','say','says','she','should',
-             'since','so','some','than','that','the','their','them','then',
+             'since','so','some','than','that','their','them','then',
              'there','these','they','this','tis','to','too','twas','us',
              'wants','was','we','were','what','when','where','which','while',
              'who','whom','why','will','with','would','yet','you','your',
